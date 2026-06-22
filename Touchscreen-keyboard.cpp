a37e49cdfc055e667f1282a05dd0793795101c7b
Raaschou <168561326+Raaschou@users.noreply.github.com>
2026-06-22 23:43:52 +0200

    Make solution touchscreen

--- Touchscreen-keyboard.cpp
+++ Touchscreen-keyboard.cpp
@@ -1,64 +1,70 @@
+#include <algorithm>
 #include <iostream>
-#include <list>
 #include <queue>
-#include <set>
 #include <vector>
 
-std::vector<std::string> grid { "qwertyuiop",
-                                "asdfghjkl",
-                                "zxcvbnm"};
+std::vector<std::string> grid { "qwertyuiop", "asdfghjkl","zxcvbnm"};
+
 
 int bfs(const char root, const char goal) {
-    std::queue<char> queue {};
-    std::vector<char> explored {};
-    queue.push(root);
-    std::set<char> set {};
+    std::queue<std::tuple<int,int, int>>queue {};
+    std::vector<std::string> explored {grid}; // blank explored letters using -
 
-    while (!queue.empty()) {
-        char current {queue.front()};
-        queue.pop();
+    for (int i {0}; i < 3; i++) {
+        for (int j {0}; j < grid[i].size(); j++){
+            if (grid[i][j] == root) {
+                queue.push({i, j, 0});
+                explored[i][j] = '-';
 
+                while (!queue.empty()) {
 
+                    auto& [x,y, step] {queue.front()};
+                    queue.pop();
+
+                    if (grid[x][y] == goal) return step;
+
+                    std::vector<std::pair<int,int>> neighbours {{x+1,y}, {x-1,y}, {x,y+1}, {x,y-1}};
+                    for (auto& [row, col] : neighbours) {
+                        if (row >= 0 && row < 3 && col >= 0 && col < grid[row].size() && explored[row][col] != '-') {
+                            queue.push({row, col, step+1});
+                            explored[row][col] = '-';
+                        }
+                    }
+                }
+            }
+        }
     }
-    return 0;
+
+    return -1;
 }
 
 int main() {
 
-
     int numCases {};
-    std::cin >> numCases;
+        std::cin >> numCases;
+
     while (numCases--) {
         std::string str{};
         int numSpellings;
+        std::vector<std::pair<int, std::string>> output {};
+
         std::cin >> str >> numSpellings;
-        std::pair<int, std::string> loc{};
+
         while (numSpellings--) {
             std::string typed;
             std::cin >> typed;
             int dist {0};
 
             for (int i {0}; i < str.length(); i++) {
-                dist += bfs(str[i], typed[i]);
+                if (str[i] != typed[i]) {
+                    dist += bfs(str[i], typed[i]);
+                }
             }
+            output.push_back({dist,typed});
+
         }
-
-
-
+        std::sort(output.begin(), output.end());
+        for (auto&[typed, dist] : output) std::cout << typed << " " << dist << std::endl;
     }
-
-
-    for (const std::vector<char>& node : grid) {
-        int n {static_cast<int>(node.capacity())};
-        for (int i {0}; i < n; i++) {
-            if (node[i] >= 'a' && node[i] <= 'z')
-
-                std::cout << node[i] <<  " ";
-            else
-                std::cout << "NULL";
-        }
-        std::cout << std::endl;
-    }
-
     return 0;
 }
