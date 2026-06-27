f94d9aaca2a64df4514cb65869cd272671d592a8
Raaschou <168561326+Raaschou@users.noreply.github.com>
2026-06-27 13:29:01 +0200

    Implement hash set sol

--- LongestConsecutiveSequence.cpp
+++ LongestConsecutiveSequence.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <map>
 #include <set>
 #include <vector>
 
@@ -7,20 +8,23 @@ using namespace std;
 
 int main() {
     vector<int> nums {2,20,4,10,3,4,5};
-    sort(nums.begin(), nums.end());
-    set<int> numsSet {nums.begin(), nums.end()};
-    int longestSeq {0};
-    int seq {0};
+    //sort(nums.begin(), nums.end());
+    set<int> numSet {nums.begin(), nums.end()};
 
-    for (int i {0}; i < nums.size()-1; i++) {
-        int d = (numsSet.begin(),i+1);
-        int x = (numsSet.begin(),i)+1;
-        if (d == x) seq++;
-        else seq = 0;
-        if (seq > longestSeq) {
-            longestSeq = seq;
+    int longestSeq {0};
+
+    for (int num : numSet) {
+        if (numSet.find(num-1) == numSet.end()) {
+            int seq {1};
+            while (numSet.find(num + seq) != numSet.end()) {
+                seq++;
+            }
+            if (seq > longestSeq) longestSeq = seq;
         }
+
     }
+
+
     std::cout << longestSeq;
     return 0;
 }
