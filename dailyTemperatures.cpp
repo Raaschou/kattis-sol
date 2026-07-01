eeee82a11ec2189ab9717a7ca191ff290083bc9c
Raaschou <168561326+Raaschou@users.noreply.github.com>
2026-07-01 22:32:07 +0200

    Do nothing

---
+++ dailyTemperatures.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    vector<int> temperatures {30,38,30,36,35,40,28};
+    vector<int> dayTillHigher {0,static_cast<int>(temperatures.size())};
+
+    for (int temp : temperatures) {
+
+    }
+}
