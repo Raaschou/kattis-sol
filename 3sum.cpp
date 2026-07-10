5216808ce2e452c36d5793d9c14e8039801462b3
Raaschou <168561326+Raaschou@users.noreply.github.com>
2026-07-10 22:09:03 +0200

    see the light

--- 3sum.cpp
+++ 3sum.cpp
@@ -3,38 +3,39 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-vector<vector<int>> ans {};
-bool checkNums(vector<int> elem) {
-    int count {0};
-    for (auto answer : ans) {
-        for (int j : answer) {
-            for (int i {0}; i < 3; i++) {
-                if (elem[i] == answer[i]) count++;
-            }
-        }
-    }
-    return count >= 3;
-}
+
 
 int main() {
 
-    vector<int> nums {0,0,0,0};
+    vector<int> nums {1,2,-2,-1};
     vector<vector<int>> expec {{-1,-1,2},
         {-1,0,1}};
+    vector<vector<int>> ans {};
 
 
-    int low = 0;
-    int high = 2;
 
-    while (low < high - 1) {
-        for (int middle {low+1}; high < nums.size(); middle++){
-            if (nums[low] + nums[middle] + nums[high] == 0 && !checkNums(vector<int>{nums[low], nums[middle], nums[high]})){
-                ans.push_back({nums[low],nums[middle],nums[high]});
+
+    sort(nums.begin(), nums.end());
+
+    for (int low {0}; low < nums.size(); low++) {
+        if (nums[low] > 0) break;
+        if (low > 0 && nums[low] == nums[low - 1]) continue;
+        int left {low + 1};
+        int right = nums.size()-1;
+        while (left < right){
+            int sum = nums[low] + nums[left] + nums[right];
+            if (sum < 0) {
+                left++;
+            } else if (sum > 0) {
+                right--;
+            } else {
+                ans.push_back({nums[low], nums[left], nums[right]});
+                left++;
+                right--;
+                while (left < right && nums[left] == nums[left - 1]) left++;
+
             }
-            high++;
         }
-        low++;
-
     }
 
 
